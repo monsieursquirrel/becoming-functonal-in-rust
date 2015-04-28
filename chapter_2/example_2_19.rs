269dd8beac9ecc190e07de26e1c535d9908af09e
monsieursquirrel <cb@lechuck.local>
2015-04-29 00:32:39 +0100

    example 2.19

---
+++ chapter_2/example_2_19.rs
@@ -0,0 +1,18 @@
+// In Rust, list operations such as filtering and mapping are usually performed
+// on iterators. These behave lazily unless you loop over them or collect() them.
+
+pub fn get_enabled_customer_names(&self) -> Vec<&str> {
+  self.all_customers.iter().filter(|customer| {
+    customer.enabled
+  }).map(|customer| {
+    &customer.name[..]
+  }).collect()
+}
+
+pub fn get_disabled_customer_names(&self) -> Vec<&str> {
+  self.all_customers.iter().filter(|customer| {
+    !customer.enabled
+  }).map(|customer| {
+    &customer.name[..]
+  }).collect()
+}
