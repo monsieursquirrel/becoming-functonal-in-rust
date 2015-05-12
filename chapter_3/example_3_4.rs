a662b435f110970b1417686e0c256c108d20164c
monsieursquirrel <conrad@bebbington.org>
2015-05-12 21:13:58 +0100

    a comment explaining use of static dispatch for closures

--- chapter_3/example_3_4.rs
+++ chapter_3/example_3_4.rs
@@ -3,6 +3,9 @@
 // parameter in order to use a mutable return type, despite not modifying the
 // data internally. This should shake out later in the book when side effects
 // are removed.
+// This function also uses a generic parameter and a where clause for the closure
+// paramete. This allows it to use static dispatch as the generic is expanded (and
+// possibly inlined) where the function call occurs.
 
 pub fn filter<T>(&mut self, test: T) -> Vec<&mut Customer>
   where T: Fn(&Customer) -> bool {
